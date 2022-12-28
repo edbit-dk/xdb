e8246cfef78947332f8731b0f6db7a0f6ff6117d
Thomas Elvin <info@datalaere.dk>
2022-12-28 14:14:33 +0100

    Update templates.php

--- assets/theme/templates.php
+++ assets/theme/templates.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
        <!-- Bootstrap css -->
     <link href="<?php echo url('/assets/theme/css/bootstrap.min.css') ?>" rel="stylesheet">
-    <link href="<?php echo url('/assets/theme/bootstrap.min.css') ?>" rel="stylesheet">
+    <link href="<?php echo url('/assets/theme/css/style.css') ?>" rel="stylesheet">
     <title>XDB - Eksamensdatabasen</title>
 </head>
 <body class="text-center">
