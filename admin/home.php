c477952a3460ddeecc360fee3295894a42b1541b
Thomas Elvin <thomas.e.s.l@outlook.dk>
2025-04-01 23:46:17 +0200

    Update home.php

--- admin/home.php
+++ admin/home.php
@@ -8,7 +8,7 @@ $teams = Team::list();
 
 <main class="form-signin w-100 m-auto" style="background-color: white; box-shadow: 11px 30px 154px 2px rgb(0 0 0 / 34%); padding: 50px;">
 <?php require 'header.php'; ?>    
-<h1 class="h3 mb-3 fw-normal"><span style="color: #015ab3; font-size: 30px; font-weight: 600;">Skole</span>Data</h1>
+<h1 class="h3 mb-3 fw-normal"><span style="color: #015ab3; font-size: 30px; font-weight: 600;">x</span>DB</h1>
 <h5>Meddelsesbog - Medarbejder</h5>
 <?php check_messages(); ?>
 <br>
